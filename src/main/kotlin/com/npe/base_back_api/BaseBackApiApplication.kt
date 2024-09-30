8a6a12e5c98f62ea77a63ab49f905b67924de1ee
Pedro Rodriguez R <pedrodriguezrubio@gmail.com>
2024-09-30 19:03:26 +0200

    First commit

---
+++ src/main/kotlin/com/npe/base_back_api/BaseBackApiApplication.kt
@@ -0,0 +1,11 @@
+package com.npe.base_back_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BaseBackApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BaseBackApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/npe/base_back_api/BaseBackApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.npe.base_back_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BaseBackApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
